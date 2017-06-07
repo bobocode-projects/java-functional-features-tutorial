3b288c38655e631e269a5abf9f8c4f2ba55bb8cc
Taras <boy4uck@gmail.com>
2017-06-07 22:49:22 +0300

    change file location

---
+++ lambda-basics/src/com/bobocode/LambdaExample_01.java
@@ -0,0 +1,43 @@
+package com.bobocode;
+
+import com.bobocode.model.Account;
+import com.bobocode.util.TestDataProvider;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+/**
+ * Parameterization in old Java version.
+ *
+ * This is a typical example of code for Java before version 8. The first class citizens in Java are objects.
+ * Therefore we always pass as methods arguments only primitive values, and objects. In other words, we add methods
+ * parameters to make methods more universal. But what if we need to parameterize the behavior? What if we need to
+ * perform different operations on account list basing on different conditions? That's called behavior parameterization.
+ *
+ * Behavior parameterization can be achieved by passing some function as methods parameter, but Java does not allow us
+ * to pass functions as method parameters. Does it?
+ *
+ * Let's try to solve this problem with old Java style. We can not pass a function, but we can create an interface
+ * with one method, and then pass its anonymous implementation with required logic. That's much more flexible right?
+ *
+ * See LambdaExample_02.java
+ *
+ * */
+public class LambdaExample_01 {
+    public static void main(String[] args) {
+        List<Account> accounts = TestDataProvider.generateAccountList();
+        giveBonusForLoyalClients(accounts, 4, BigDecimal.valueOf(50));
+    }
+
+    private static void giveBonusForLoyalClients(List<Account> accounts, int yearsWithBank, BigDecimal bonus) {
+        for (Account account : accounts) {
+            // todo: HARD CODED condition should be passed as method argument
+            if (Period.between(account.getCreationDate(), LocalDate.now()).getYears() >= yearsWithBank) {
+                // todo: HARD CODED operation should be passed as method argument
+                account.setBalance(account.getBalance().add(bonus));
+            }
+        }
+    }
+}
---
+++ lambda-basics/src/com/bobocode/LambdaExample_03.java
@@ -0,0 +1,42 @@
+package com.bobocode;
+
+import com.bobocode.functions.Condition;
+import com.bobocode.functions.Operation;
+import com.bobocode.model.Account;
+import com.bobocode.util.TestDataProvider;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+import static java.time.Period.between;
+
+/**
+ * Behavior parameterization with Lambdas.
+ *
+ * Lambdas allow you to provide only required specific code, and make code clear. Lambda is an anonymous method, but
+ * it is always assigned to some interface. It can be assigned to an interface variable, or passes as method argument.
+ * Lambda should be always compatible with its target type, which is specified by some interface. Such interface
+ * is called Functional interface, because it represents only one function that is defined by one abstract method.
+ *
+ * See LambdaExample_04.java for different target typing examples
+ *
+ */
+public class LambdaExample_03 {
+    public static void main(String[] args) {
+        List<Account> accounts = TestDataProvider.generateAccountList();
+        processAccountWithoutLambdas(accounts,
+                a -> Period.between(a.getCreationDate(), LocalDate.now()).getYears() > 4,
+                a -> a.setBalance(a.getBalance().add(BigDecimal.valueOf(50))));
+    }
+
+    private static void processAccountWithoutLambdas(List<Account> accounts, Condition<Account> condition,
+                                                     Operation<Account> operation) {
+        for (Account account : accounts) {
+            if (condition.isTrue(account)) {
+                operation.apply(account);
+            }
+        }
+    }
+}
